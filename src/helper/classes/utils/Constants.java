af30de9a36d54468661308609a78955451f62583
Ben <benhaimerl@googlemail.com>
2024-12-13 18:15:51 +0100

    Constants

---
+++ src/helper/classes/utils/Constants.java
@@ -0,0 +1,15 @@
+package helper.classes.utils;
+
+public class Constants {
+	
+	public static final String DRUIDCOLOR = "#FF7D0A";
+	public static final String HUNTERCOLOR = "#A9D271";
+	public static final String PALADINCOLOR = "#F58CBA";
+	public static final String MAGECOLOR = "#40C7EB";
+	public static final String PRIESTCOLOR = "#FFFFFF";
+	public static final String SHAMANCOLOR = "#0070DE";
+	public static final String ROGUECOLOR = "#FFF569";
+	public static final String WARLOCKCOLOR = "#8787ED";
+	public static final String WARRIORCOLOR = "#C79C6E";
+
+}
